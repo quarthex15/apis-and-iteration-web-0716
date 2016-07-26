b40e5a80d79b3bd7f816209feb0416300da1f017
Andrew Klein <andrew.klein.j@gmail.com>
2016-07-26 13:17:27 -0400

    change gets.chomp to gets.strip

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -5,7 +5,7 @@ end
 
 def get_character_from_user
   puts "please enter a character"
-  character = gets.chomp
+  character = gets.strip
   character.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end

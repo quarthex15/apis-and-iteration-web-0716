82f1fd9c35f734527012b9e0780bc48f3ee1358e
marya314 <mary.boyd314@gmail.com>
2016-07-26 12:09:11 -0400

    get character method

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -5,6 +5,9 @@ end
 
 def get_character_from_user
   puts "please enter a character"
-  # use gets to capture the user's input. This method should return that input, downcased. 
+  character = gets.chomp
+  character.downcase
+  # use gets to capture the user's input. This method should return that input, downcased.
 end
 
+get_character_from_user

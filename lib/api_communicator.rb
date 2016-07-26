a8691afdfb7783813361776fba538929a80aa013
marya314 <mary.boyd314@gmail.com>
2016-07-26 18:15:33 -0400

    Add page functionality and seperate methods

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,20 +2,50 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
+def get_character_hash_from_api
   #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
-  character_data = character_hash["results"].select do |char|
+  #all_characters = RestClient.get("http://www.swapi.co/api/people/")
+  #initial_hash = JSON.parse(all_characters)
+  #pages = initial_hash["count"]
+
+  character_hashes = []
+
+  last_page = 0
+  page_number = 1
+  until last_page == 1
+
+    all_characters_on_page = RestClient.get("http://www.swapi.co/api/people/?page=#{page_number}")
+    page_number += 1
+    page_character_hash = JSON.parse(all_characters_on_page)
+    #binding.pry
+    last_page = 1 if page_character_hash["next"] == nil
+
+    page_character_hash["results"].each do |char|
+      character_hashes << char
+    end
+    #binding.pry
+  end
+
+  character_hashes
+end
+
+
+def find_character(character, characters_array)
+  #binding.pry
+  character_data = characters_array.select do |char|
+    #binding.pry
     character==char["name"].downcase
   end
-  film_urls = character_data[0]["films"]
+  #binding.pry
+end
 
+def get_film_hashes_from_api(character_data)
+  #binding.pry
+  film_urls = character_data[0]["films"]
   film_urls.collect do |film_url|
     JSON.parse(RestClient.get(film_url))
   end
-
+end
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   # return value of this method should be collection of info about each film.
@@ -23,11 +53,11 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
-end
 
-def parse_character_movies(films_hash)
+
+def parse_character_movies(films_array)
   output_array = []
-  films_hash.each do |film|
+  films_array.each do |film|
     #binding.pry
     title = film["title"]
     director = film["director"]
@@ -37,7 +67,6 @@ def parse_character_movies(films_hash)
     #Put puts here'
 
     output_array << "Episode #{episode_number}: #{title}, directed by #{director}, released in #{year_released}"
-
   end
   #binding.pry
   output_array.sort_by! do |film|
@@ -48,8 +77,10 @@ def parse_character_movies(films_hash)
 end
 
 def show_character_movies(character)
-  films_hash = get_character_movies_from_api(character)
-  parse_character_movies(films_hash)
+  characters_array = get_character_hash_from_api
+  character_data = find_character(character, characters_array)
+  films_array = get_film_hashes_from_api(character_data)
+  parse_character_movies(films_array)
 end
 
 ## BONUS
